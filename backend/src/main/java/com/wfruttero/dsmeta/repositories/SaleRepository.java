d5c24c40ab6ffe9a70a7626781bf3fd65bb97b90
willfruttero <willfruttero@outlook.com>
2022-07-13 19:28:28 -0300

    API test

---
+++ backend/src/main/java/com/wfruttero/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.wfruttero.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wfruttero.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
