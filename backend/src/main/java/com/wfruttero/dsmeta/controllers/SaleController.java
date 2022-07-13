842701c3bdcb69675aae748015ef51c312cc0fcd
willfruttero <willfruttero@outlook.com>
2022-07-13 20:05:55 -0300

    Date select

--- backend/src/main/java/com/wfruttero/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/wfruttero/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.wfruttero.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wfruttero.dsmeta.entities.Sale;
@@ -13,13 +14,13 @@ import com.wfruttero.dsmeta.services.SaleService;
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
-	
+
 	@Autowired
 	private SaleService service;
-	
-	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();		
-	}
 
+	@GetMapping
+	public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
+	}
 }
